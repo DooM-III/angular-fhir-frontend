5539d3c8ce59a6b68ea5035e7dac7f975212a961
Ludovic Lecoq <ludovic.lecoq@atos.net>
2023-08-01 19:55:59 +0200

    v1 app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './models/face-snap.model';
+import { Observable, filter, interval, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'snapface';
+
+export class AppComponent implements OnInit {
+  faceSnaps!: FaceSnap[];
+  interval$!: Observable<string>;
+
+  ngOnInit() {
+    this.interval$ = interval(1000).pipe(
+      filter(value => value % 3 === 0),
+      map(value => value % 2 === 0 ?
+          `Je suis ${value} et je suis pair` :
+          `Je suis ${value} et je suis impair`
+      ),
+      tap(text => this.logger(text))
+      );
+    }
+
+  logger(text: string): void {
+    console.log(`Log: ${text}`);
+    }
 }
---
+++ src/app/models/face-snap.model.ts
@@ -0,0 +1,26 @@
+// export class FaceSnap {
+//     title: string;
+//     description: string;
+//     createdDate: Date;
+//     snaps: number;
+//     imageUrl: string;
+    
+//     constructor(title: string, description: string, imageUrl: string, createdDate: Date, snaps: number) {
+//       this.title = title;
+//       this.description = description;
+//       this.imageUrl = imageUrl;
+//       this.createdDate = createdDate;
+//       this.snaps = snaps;
+//     }
+//   }
+
+export class FaceSnap {
+    id!: number;
+    title!: string;
+    description!: string;
+    imageUrl!: string;
+    createdDate!: Date;
+    snaps!: number;
+    location?: string;
+  }
+
